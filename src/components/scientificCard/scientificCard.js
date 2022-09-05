b6d20e9dbb3abcb19c6af150eca6dbeef985b564
henrique <hickm018@gmail.com>
2022-09-05 18:14:35 -0300

    feat: create logic favorite article

---
+++ src/components/scientificCard/scientificCard.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+
+export default function ScientificCard({title, type, description, url, authors}) {
+
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const favorite = JSON.parse(localStorage.getItem('favorite')) || [];
+    favorite.forEach((item) => {
+      if (item.title === title && item.type === type) {
+        setChecked(true);
+      }
+    });
+  }, []);
+  const setFavoriteLocalStorage = () => {
+      const favoriteArticle = {
+        title,
+        type,
+        description,
+        url,
+        authors,
+      };
+  
+      const favorite = JSON.parse(localStorage.getItem('favorite'));
+  
+      if (!favorite) {
+        localStorage.setItem('favorite', JSON.stringify([favoriteArticle]));
+      };
+    
+      if (favorite) {
+        const favoriteExist = favorite.find((item) => item.title === title);
+        if (!favoriteExist) {
+          localStorage.setItem('favorite', 
+          JSON.stringify([favoriteArticle, ...favorite]));
+        };
+        };
+    };
+
+    const removeFavoriteLocalStorage = () => {
+      const favorite = JSON.parse(localStorage.getItem('favorite'));
+      const favoriteFilter = favorite.filter((item) => item.title !== title);
+      localStorage.setItem('favorite', JSON.stringify(favoriteFilter));
+    };
+
+  return (
+    <div>
+      <h1>{ title }</h1>
+      <h3>{ type }</h3>
+      <h4>{ authors[0] }</h4>
+      <p>{ description }</p>
+      {
+        url.map((link) => (
+          <a href={link} target="_blank" rel="noreferrer">Link</a>))
+      }
+      <input
+        type="checkbox"
+        id="heart"
+        checked={checked}
+        onChange={({ target }) => {
+          setChecked(target.checked);
+          if (target.checked) {
+            setFavoriteLocalStorage();
+          } else {
+            removeFavoriteLocalStorage();
+          }
+        }}
+      />
+    </div>
+  );
+}
