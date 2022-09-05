081eb4dfd609d4f98405ac2e1936b2212f7e256c
henrique <hickm018@gmail.com>
2022-09-05 18:14:18 -0300

    feat: change folder organization

--- src/components/scientificContainer.js
+++ src/components/scientificContainer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import initialContent from '../API/initialContent';
-import ScientificCard from './scientificCard';
+import ScientificCard from './scientificCard/scientificCard';
+
 
 function ScientificContainer() {
 
