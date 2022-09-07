ca6118427046ac56ae45887cdc2c201fa12e9f6d
henrique <hickm018@gmail.com>
2022-09-07 20:45:39 -0300

    style: add style in header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header({currentPage}) {
   return (
-    <header>
+    <header className='header-container'>
       <h1>Scientific Articles</h1>
       {
         currentPage === 'home' ? 
-          <Link to="/favorites">Favorites</Link> :
-          <Link to="/">Home</Link>
+          <Link className='btn-navigation' to="/favorites">Favorites</Link> :
+          <Link className='btn-navigation' to="/">Home</Link>
       }
     </header>
   );
